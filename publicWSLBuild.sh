3c0f515da958805e91084112859fd37df1763593
Kacy Stocks <kstocks0811@gmail.com>
2021-03-08 12:37:43 -0700

    cleaned up script, switched to printf

--- publicWSLBuild.sh
+++ publicWSLBuild.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
+set -e
+set -x
 cd || exit 1
-apt install sudo
-err=$?
-if [ $err != 0 ] ; then
-    echo "error installing sudo: $err"
-    exit 1
-fi
 apt update
 apt dist-upgrade -y
-err=$?
-if [ $err != 0 ] ; then
-    echo "error upgrading: $err"
-    exit 1
-fi
 apt install golang-go
-err=$?
-if [ $err != 0 ] ; then 
-    echo "error installing go: $err"
-    exit 1
-fi
-echo "export GOROOT=/usr/local/go
+printf "export GOROOT=/usr/local/go
 export GOPATH=\$HOME/go
 export PATH=\$GOPATH/bin:#GOROOT/BIN:\$PATH" >> ~/.bashrc
 git --version
@@ -28,71 +14,26 @@ if [ $err = 127 ] ; then
     apt install git
     err=0
 fi
-echo "\n\n\n************** Attention **************"
-echo "You should probably configure git:"
-echo "git config --global user.name \"myusername\""
-echo "git config --global user.email \"myemail@email.com\""
-echo "\n\n\n************** Attention **************"
+printf "\n\n\n************** Attention **************"
+printf "You should probably configure git:"
+printf "git config --global user.name \"myusername\""
+printf "git config --global user.email \"myemail@email.com\""
+printf "\n\n\n************** Attention **************"
 
 git config --global core.autocrlf false
 cd ~ || exit 1
 mkdir .ssh
 chmod 700 .ssh
 apt install python3 python3-pip ipython3
-err=$?
-if [ $err != 0 ] ; then
-    echo "error installing python 3, pip, or ipython 3: $err"
-    exit 1
-fi
-sudo apt install g++ freeglut3-dev libglu1-mesa-dev libgl1-mesa-dev valgrind libgtest-dev
-if [ $err != 0 ] ; then
-    echo "error installing g++, glut, glut, mesa, valgrind, or libgtest: $err"
-    exit 1
-fi
-sudo apt install gcc-multilib g++-multilib
-if [ $err != 0 ] ; then
-    echo "error installing gcc or g++ $err"
-    exit 1
-fi
 apt install shellcheck
-err=$?
-if [ $err != 0 ] ; then
-    echo "error installing shellcheck: $err"
-    exit 1
-fi
 #### feel free to install latex, if you are going to be taking comp theory
 # apt install texlive-latex-recommended texlive-metapost texlive-fonts-recommended
-# err=$?
-# if [ $err != 0 ] ; then
-#     echo "error installing LaTeX: $err"
-#     exit 1
-# fi
-apt install vim-gtk
-err=$?
-if [ $err != 0 ] ; then
-    echo "error installing vim-gtk: $err"
-    exit 1
-fi
+# t install vim-gtk
 curl https://cli-assets.heroku.com/install.sh | sh
-err=$?
-if [ $err != 0 ] ; then
-    echo "error installing heroku: $err"
-    exit 1
-fi
 apt install build-essential
-err=$?
-if [ $err != 0 ] ; then
-    echo "error installing build-essential: $err"
-    exit 1
-fi
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash
-err=$?
-if [ $err != 0 ] ; then
-    echo "error installing npm: $err"
-    exit 1
-fi
 apt update && apt dist-upgrade -y
-echo "---------------------
+printf "---------------------
 It is suggested you run these commands with your
 username once this script has finished:
 cd .ssh || exit 1
@@ -100,23 +41,13 @@ cp /mnt/c/Users/{insert your username here}/.ssh/id_rsa* .
 chmod 600 id_rsa
 chmod 644 id_rsa.pub
 ---------------------"
-echo "After running the above suggested commands,
+printf "After running the above suggested commands,
 Please copy your rsa key and paste it to
 your github's authorized keys using this command:
 cat ~/.ssh/id_rsa.pub
 ---------------------"
 apt install zsh -y
-err=$?
-if [ $err != 0 ] ; then
-    echo "error installing zsh: $err"
-    exit 1
-fi
-echo "if [ -t 1 ] ; then
+printf "if [ -t 1 ] ; then
 exec zsh
 fi" >> ~/.bashrc
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" -y
-err=$?
-if [ $err != 0 ] ; then
-    echo "error installing oh my zsh: $err"
-    exit 1
-fi
